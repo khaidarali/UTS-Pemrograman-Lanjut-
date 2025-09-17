8c9f30161aaacbc22afda050c5a66d4d700146eb
khaidarali <152408107+khaidarali@users.noreply.github.com>
2025-09-17 09:54:09 +0700

    Update BmiKalkulator (3) (1).java

--- BmiKalkulator (3) (1).java
+++ BmiKalkulator (3) (1).java
@@ -1,38 +1,34 @@
 import java.util.Scanner;
 
 public class BmiKalkulator {
-    public static void main(String[] args) 
-    {
+    public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        System.out.println("Masukkan tinggi badan: ");
+        System.out.println("Masukkan tinggi badan (cm): ");
         double tinggiBadan = input.nextDouble();
 
-        System.out.println("Masukan berat badan: ");
+        System.out.println("Masukkan berat badan (kg): ");
         double beratBadan = input.nextDouble();
 
         double tinggiBadanM = tinggiBadan / 100.0;
-
         double bmi = hitungBMI(tinggiBadanM, beratBadan);
 
         System.out.println("BMI kamu adalah: " + bmi);
 
-        
-        }
-        else if (bmi >= 18.5 && bmi <= 24.9)
-        {
+        if (bmi < 18.5) {
+            System.out.println("Kamu kekurangan berat badan");
+        } else if (bmi >= 18.5 && bmi <= 24.9) {
             System.out.println("Berat badan kamu normal");
-        }
-        else if (bmi >= 25 && bmi <= 29.9)
-        {
+        } else if (bmi >= 25 && bmi <= 29.9) {
             System.out.println("Kamu kelebihan berat badan");
-        }
-        else
-        {
+        } else {
             System.out.println("Kamu obesitas");
         }
+
+        input.close();
     }
-    private static double hitungBMI(double tinggiBadanM, double beratBadan)
-    {
+
+    private static double hitungBMI(double tinggiBadanM, double beratBadan) {
         return beratBadan / (tinggiBadanM * tinggiBadanM);
-    sego pecel
+    }
+}
